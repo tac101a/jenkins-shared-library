1da8e523d2d434c49ef340ffc2a57cb607a1906e
TAC <anh2017ntp@gmail.com>
2026-06-02 09:37:14 +0700

    Use oc patch to set nodeSelector on deployment
    Replace oc set node-selector with oc patch to apply the nodeSelector directly to the deployment's pod template. Uses a JSON merge patch to set kubernetes.io/hostname=cpp00061764l on the template spec, ensuring the selector is applied to pods (and compatible with newer oc/openshift behavior) without altering other deployment settings.

--- vars/deployOpenshift.groovy
+++ vars/deployOpenshift.groovy
@@ -43,7 +43,7 @@ def call(Map config) {
                     else
                         oc create deployment "$APP_NAME" --image="$IMAGE_TAG"
                     fi
-                    oc set node-selector deployment/"$APP_NAME" kubernetes.io/hostname="cpp00061764l"
+                    oc patch deployment/"$APP_NAME" --patch '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/hostname":"cpp00061764l"}}}}}'
 
                     echo "5. Publish Service/Route..."
                     oc expose deployment "$APP_NAME" --port=8080 --target-port=8080 >/dev/null 2>&1 || true
